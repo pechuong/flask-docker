68ac21c74a71293eefb59fb1d23152c65d0ce6b3
Peter <peter@PC.local>
2021-05-20 23:58:43 -0700

    thing works but didn't use it in anything so tried it here

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ import time
 from flask import Flask
 import requests
 import pytest
+import character
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-  return 'Hi! I\'m Mr. Meeseeks! Look at me!!\n Existence is pain!'
+  test_json = character.get_character("Morty Smith")
+  alive = character.is_alive(test_json)
+  test_str = "Morty Smith is "
+  if (alive):
+    test_str += "Alive"
+  else:
+    test_str += "Dead"
+  return test_str
+  # return 'Hi! I\'m Mr. Meeseeks! Look at me!!\n Existence is pain!'
 
 
 if __name__ == '__main__':
